e9384f661f996c7f50f4625964f282629290e3cc
felinho927 <felix.lentze@gmx.de>
2024-07-04 10:33:36 +0200

    finished 1/y proof

--- src/Continuity.lean
+++ src/Continuity.lean
@@ -128,30 +128,40 @@ example (x : ℝ) (hx : x ≠ 0) : IsContinuousAt { x | x ≠ 0} (fun y ↦ 1 / 
   intro ε hε
   let δ : ℝ := ε * |x| * |x| / 2 ⊓ |x|/2
   use δ
-  have hd : 0 < δ := by simp [δ]; positivity
+  have hd : 0 < δ := by
+    simp [δ]
+    constructor
+    · exact by positivity
+    · exact hx
   have hd' : δ ≤ ε * |x| * |x| / 2 := inf_le_left
+  have hd'' : δ ≤ |x| / 2 := inf_le_right
   refine ⟨hd, ?_⟩
   intro y hy hyd
-  have h1: |x| > 0 := by positivity
-  have h2: |y| ≠ 0 := abs_ne_zero.mpr hy
-  have h3: |y| > 0 := by positivity
-  have h4: |x| * |y| > 0 := by exact Real.mul_pos h1 h3
-  have h5: |x| / 2 ≤ |y| := by sorry
+  have h1 : |x| > 0 := by positivity
+  have h2 : |y| ≠ 0 := abs_ne_zero.mpr hy
+  have h3 : |y| > 0 := by positivity
+  have h4 : |x| * |y| > 0 := by exact Real.mul_pos h1 h3
+  have h5 : |x| / 2 ≤ |y| := by
+    calc |x| / 2 = |x| - |x| / 2 := by ring_nf
+      _ ≤ |x| - δ := by linarith [hyd]
+      _ ≤ |x| - |x - y| := by linarith [hd'']
+      _ ≤ |x - (x - y)| := abs_sub_abs_le_abs_sub x (x - y)
+      _ = |y| := by ring_nf
   have h6 : δ ≤ ε * |x| * |x| / 2 := inf_le_left
   have h7 : 0 ≤ ε * |x| * |x| / 2 := le_trans (le_of_lt hd) h6
   have h8 : 0 < |x| * (|x| / 2) := by exact mul_pos h1 (half_pos h1)
   have h9 : |x| * (|x| / 2) ≤ |x| * |y| := mul_le_mul_of_nonneg_left h5 (le_of_lt h1)
   calc |1/x - 1/y| = |(1 * y - 1 * x) / (x * y)| := by rw [div_sub_div 1 1 hx hy]; ring_nf
     _ = |(y - x) / (x * y)| := by ring_nf
-    _ = |y - x| / |x * y| := abs_div (y - x) (x * y)
+    _ = |y - x| / |x * y| := by rw [abs_div]
     _ = |x - y| / |x * y| := by rw [abs_sub_comm]
-    _ = |x - y| / (|x| * |y|) := by sorry
+    _ = |x - y| / (|x| * |y|) := by rw [abs_mul]
     _ < δ / (|x| * |y|) := (div_lt_div_right h4).mpr hyd
-    _ ≤ δ / (|x| * |x|/2) := div_le_div _ _ _ _
-    _ ≤ (ε * |x| * |x|/2) / (|x| * |x|/2) := sorry
+    _ ≤ δ / (|x| * (|x|/2)) := (div_le_div_left hd h4 h8).mpr h9
+    _ ≤ (ε * |x| * |x|/2) / (|x| * (|x|/2)) := (div_le_div_right h8).mpr hd'
+    _ = ε * (|x| * |x|/2) / (|x| * (|x|/2)) := by ring_nf
     _ = ε := by field_simp
 end IsContinuousAt
-
 /-
 If you want to read the documentation of a specific tactic, you can use:
 -/
