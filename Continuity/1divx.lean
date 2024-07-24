90869f6fcd8eb63219a5137da522ed7904b907ea
felinho927 <felix.lentze@gmx.de>
2024-07-24 10:04:29 +0200

    add some minor changes to 1divx

--- Continuity/1divx.lean
+++ Continuity/1divx.lean
@@ -1,4 +1,3 @@
-import Mathlib.Topology.Instances.Real
 import Mathlib.Data.Real.Basic
 import Mathlib.Topology.Instances.Real
 import Mathlib.Topology.Basic
@@ -11,17 +10,15 @@ example : ¬ ContinuousAt (fun x ↦ 1 / x) 0 := by
   intro h_cont
   let x_n := fun n => 1 / (n + 1 : ℝ)
   have h_tendsto_x_n : Filter.Tendsto x_n Filter.atTop (nhds 0) := by
-    sorry
+    intro ε hε
   have h_not_tendsto : ¬Filter.Tendsto (fun n => 1 / x_n n) Filter.atTop (nhds 0) := by
     intro h_tendsto
     -- Berechne 1 / x_n n
-    have h_eq : ∀ n, 1 / x_n n = n + 1 := by
-      intro n
-      simp [x_n]
     have h_diverges : Filter.Tendsto (fun n => n + 1) Filter.atTop Filter.atTop := by
-      sorry
+      exact tendsto_add_atTop_nat 1
     -- Da h_tendsto eine Konvergenz gegen 0 verlangt, ist dies ein Widerspruch
-    sorry
+    simp[x_n] at h_tendsto
+    sorry 
   have h_not_cont : ¬Filter.Tendsto (fun x => 1 / x) (nhds 0) (nhds 0) := by
     sorry
   contradiction
